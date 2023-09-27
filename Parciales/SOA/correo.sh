1d0a228db122c6513b84dc01e027ad41cec38a24
milena confeggi <128049813+MilenaConfeggi@users.noreply.github.com>
2023-09-27 14:24:33 -0400

    Create correo.sh

---
+++ Parciales/SOA/correo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Recorro el /etc/passwd
+for linea in $(cat /etc/passwd); do
+  #Me guardo ruta que se encuentra en la posicion 6
+  ruta=$(echo $linea | cut -d: f6)
+  nom=$(echo $linea | cut -d: f1)
+  bash=$(echo $linea | cut -d f7)
+  #Busco en la ruta un tipo directorio con nombre mailDir
+  #Cuento la cantidad y si hay uno es que existe
+  #Verifico que bash= /bin/bash
+  if [$(sudo find $ruta -type d -name mailDir | wc -l) -eq 1] && [$bash -eq /bin/bash]>
+    echo "$nom" >> /var/listado.tx 
